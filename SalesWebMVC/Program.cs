0bf287158b946b0ec2dbc37aa233c4db2cd40ca8
Fabio Marques <engfabiom@gmail.com>
2024-04-09 10:52:06 +0900

    adapted Seeding Service

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -4,12 +4,15 @@ namespace SalesWebMVC {
     public class Program {
         public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
+
+            //builder services
             builder.Services.AddDbContext<SalesWebMVCContext>(options =>
                 options.UseMySql(
                     builder.Configuration.GetConnectionString("SalesWebMVCContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMVCContext' not found."),
                     ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("SalesWebMVCContext")),
                     b => b.MigrationsAssembly("SalesWebMVC"))
                 );
+            builder.Services.AddScoped<ServiceSeeding>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -21,6 +24,15 @@ namespace SalesWebMVC {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+            else {
+                // https://learn.microsoft.com/en-us/aspnet/core/data/ef-rp/intro?view=aspnetcore-6.0&tabs=visual-studio#seed-the-database
+                using (var scope = app.Services.CreateScope()) {
+                    var services = scope.ServiceProvider;
+                    var context = services.GetRequiredService<SalesWebMVCContext>();
+                    var seedingService = new ServiceSeeding(context);
+                    seedingService.Seed();
+                }
             }
 
             app.UseHttpsRedirection();
