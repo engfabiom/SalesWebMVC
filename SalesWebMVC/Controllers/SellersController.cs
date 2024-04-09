944eaeb025f200dcc88e3f48cc097ef4521b7449
Fabio Marques <engfabiom@gmail.com>
2024-04-09 15:12:34 +0900

    SellersController

---
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMVC.Controllers {
+    public class SellersController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
